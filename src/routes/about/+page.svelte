8b908f744b6b78a834ce43ebbce5594d82304b4c
nadiyaali <nadiya.ali@gmail.com>
2024-10-26 21:34:18 +1100

    Updated About text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,16 @@
         <p><b>Make City Green</b> has been designed to take the imput from citizens on how to make our cities
         greener and fight climate change. Players can view the Urban Heat Island(UHI) index for each Mesh Block in
         Melbourne City and plant trees inorder to combat the heat. The Crop Stop can be visited to collect seeds and fertilizer
-        which can make our trees grow faster.</p>
+        which can make the trees grow faster. "Start Game" locates the player and zooms in. Player can now click anywhere and start planting trees</p>
+        <br>
+        <b>Group Members</b>
+        <p>
+            Nadia Omair Sharif (s4081850)
+            <br>
+            Dona Sandani Walpita (S4078835)
+            <br>
+            Aleena Thomas
+        </p>
     </div>
     <div class="col-span-1 md:col-span-1  p-4"></div>
 </div>
