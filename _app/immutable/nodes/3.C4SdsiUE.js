44e919ea14d94010875d95a152a835e1bbe0a3d0
nadiyaali <nadiya.ali@gmail.com>
2024-11-07 13:54:23 +1100

    Updates

---
+++ _app/immutable/nodes/3.C4SdsiUE.js
@@ -0,0 +1,11 @@
+import{s as i,f as r,g as c,v as l,j as d,i as p,z as a,d as h}from"../chunks/scheduler.ieuXqiZN.js";import{S as m,i as b}from"../chunks/index.Dm-7lw2j.js";function u(o){let e,n=`<div class="col-span-1 md:col-span-1 p-4"></div> <div class="col-span-2 md:col-span-1 p-4"><h1 class="font-bold w-28">City Green</h1> <br/> <p>In City Green, you step into the shoes of a city planner on a mission to combat climate change
+            and fight the urban heat island effect. Set in your own city, your goal is to strategically 
+            plant trees to increase tree canopy cover and create green spaces to cool it down.</p> <br/> <p><b>City Green</b> has been created to take the imput from citizens on how to make our cities
+        greener and fight climate change. Players can view the Urban Heat Island(UHI) index for each Mesh Block in
+        Melbourne City and plant trees in order to combat the heat. Seed Stops can be visited to collect seeds and magic grow
+        which can make the tree canopy grow faster. &quot;Start Game&quot; locates the player and zooms in. 
+        Player can now click on any block to view UHI index and double click to plant a tree</p> <br/> <b>Group Members</b> <p>Nadia Omair Sharif (s4081850)
+            <br/>
+            Dona Sandani Walpita (s4078835)
+            <br/>
+            Aleena Thomas</p></div> <div class="col-span-1 md:col-span-1 p-4"></div>`;return{c(){e=r("div"),e.innerHTML=n,this.h()},l(t){e=c(t,"DIV",{class:!0,"data-svelte-h":!0}),l(e)!=="svelte-1r9ove9"&&(e.innerHTML=n),this.h()},h(){d(e,"class","grid grid-cols-3 gap-4")},m(t,s){p(t,e,s)},p:a,i:a,o:a,d(t){t&&h(e)}}}class v extends m{constructor(e){super(),b(this,e,null,u,i,{})}}export{v as component};
