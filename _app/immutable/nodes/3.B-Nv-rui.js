661804f470456eaac4e894326d1de39dbb260055
nadiyaali <nadiya.ali@gmail.com>
2024-11-07 13:17:24 +1100

    Updates

---
+++ _app/immutable/nodes/3.B-Nv-rui.js
@@ -0,0 +1,8 @@
+import{s as o,f as r,g as c,v as l,j as d,i as p,z as t,d as h}from"../chunks/scheduler.ieuXqiZN.js";import{S as m,i as b}from"../chunks/index.Dm-7lw2j.js";function v(s){let e,n=`<div class="col-span-1 md:col-span-1 p-4"></div> <div class="col-span-2 md:col-span-1 p-4"><p><b>Make City Green</b> has been designed to take the imput from citizens on how to make our cities
+        greener and fight climate change. Players can view the Urban Heat Island(UHI) index for each Mesh Block in
+        Melbourne City and plant trees in order to combat the heat. The Seed Stop can be visited to collect seeds and magic grow
+        which can make the tree canopy grow faster. &quot;Start Game&quot; locates the player and zooms in. Player can now click anywhere to view the Urban Heat Index(UHI) and double click to plant at tree</p> <br/> <b>Group Members</b> <p>Nadia Omair Sharif (s4081850)
+            <br/>
+            Dona Sandani Walpita (s4078835)
+            <br/>
+            Aleena Thomas</p></div> <div class="col-span-1 md:col-span-1 p-4"></div>`;return{c(){e=r("div"),e.innerHTML=n,this.h()},l(a){e=c(a,"DIV",{class:!0,"data-svelte-h":!0}),l(e)!=="svelte-b63wsn"&&(e.innerHTML=n),this.h()},h(){d(e,"class","grid grid-cols-3 gap-4")},m(a,i){p(a,e,i)},p:t,i:t,o:t,d(a){a&&h(e)}}}class f extends m{constructor(e){super(),b(this,e,null,v,o,{})}}export{f as component};
